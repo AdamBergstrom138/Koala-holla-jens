4d8c83385bfbadccd73e7debdd847ed137a8ab48
Adam Bergstrom <abx1138@gmail.com>
2022-12-15 13:40:35 -0600

    installed pg, express, body-parser

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const pool = require('./modules/pool.js');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const koalaRouter = require('./routes/koala.router')
